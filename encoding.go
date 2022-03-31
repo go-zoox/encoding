2a91644f51c7936bf8e4103319a38110668694f3
whwewant <tobewhatwewant@outlook.com>
2022-03-31 17:56:55 +0800

    ci: lint error, give up to export internal packages

--- encoding.go
+++ encoding.go
@@ -1,11 +1,5 @@
 package encoding
 
-import (
-	"github.com/go-zoox/encoding/json"
-	"github.com/go-zoox/encoding/toml"
-	"github.com/go-zoox/encoding/yaml"
-)
-
 // Encoding is an interface for encoding/decoding.
 type Encoding interface {
 	// Encode returns the X encoding of the data.
@@ -13,12 +7,3 @@ type Encoding interface {
 	// Decode decodes the X-encoded data and stores the result in the value pointed to by v.
 	Decode([]byte) ([]byte, error)
 }
-
-// Json exports from encoding/json
-var Json json.JSON
-
-// Yaml exports from encoding/yaml
-var Yaml yaml.YAML
-
-// Toml exports from encoding/toml
-var Toml toml.TOML
