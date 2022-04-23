16ed0efaa5a1b9a575a810ae35cca9176650f9c9
whwewant <tobewhatwewant@outlook.com>
2022-04-23 20:44:31 +0800

    feat: add ini support

--- ini/ini.go
+++ ini/ini.go
@@ -11,23 +11,23 @@ func New() *INI {
 	return &INI{}
 }
 
-// Encode returns the JSON encoding of the data.
+// Encode returns the INI encoding of the data.
 func (j *INI) Encode(v interface{}) ([]byte, error) {
 	return goini.Marshal(v)
 }
 
-// Decode decodes the JSON-encoded data and stores the result in the value pointed to by v.
+// Decode decodes the INI-encoded data and stores the result in the value pointed to by v.
 func (j *INI) Decode(raw []byte, v interface{}) error {
 	return goini.Unmarshal(raw, v)
 }
 
-// Encode returns the JSON encoding of the data.
+// Encode returns the INI encoding of the data.
 //	Static Method
 func Encode(v interface{}) ([]byte, error) {
 	return New().Encode(v)
 }
 
-// Decode decodes the JSON-encoded data and stores the result in the value pointed to by v.
+// Decode decodes the INI-encoded data and stores the result in the value pointed to by v.
 //	Static Method
 func Decode(raw []byte, v interface{}) error {
 	return New().Decode(raw, v)
